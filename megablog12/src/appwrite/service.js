a8510acd80a15c471c4a65b5e61082dcbb3e4380
= <=>
2024-02-02 23:54:11 +0530

    redux toolkit store created in project

--- megablog12/src/appwrite/service.js
+++ megablog12/src/appwrite/service.js
@@ -1,5 +1,5 @@
 import config from '../config'
-import {Client, Databases, Storage} from 'appwrite';
+import {Client, Databases, Storage, Query, ID} from 'appwrite';
 
 
 export class Service {
@@ -52,5 +52,81 @@ export class Service {
         } catch (error) {
             console.log("Appwrite service :: updatePost :: error :: ", error);
         }
+    }
+
+
+    async deletePost(slug){
+        try {
+            await this.databases.deleteDocument(
+                config.appwriteDatabaseId,
+                config.appwriteCollectionId,
+                slug
+            )
+            return true;
+        } catch (error) {
+            console.log("Appwrite service :: deletePost :: error :: ", error);
+            return false;
+        }
+    }
+
+    async getPost(slug){
+        try {
+            return await this.databases.getDocument(
+                config.appwriteDatabaseId,
+                config.appwriteCollectionId,
+                slug
+            )
+        } catch (error) {
+            console.log("Appwrite service :: getPost :: error :: ", error);
+            return false;
+        }
+    }
+
+    async getPosts(queries = [Query.equal("status", "active")]){
+        try {
+            return await this.databases.listDocuments(
+                config.appwriteDatabaseId,
+                config.appwriteCollectionId,
+                queries,
+            )
+        } catch (error) {
+            console.log("Appwrite service :: getPosts :: error :: ", error);
+            return false;
+        }
+    }
+
+    // file uploaded service methods
+    async uploadFile(file){
+        try {
+            return await this.bucket.createFile(
+                config.appwriteBucketId,
+                ID.unique(),
+                file
+            )
+        } catch (error) {
+            console.log("Appwrite service :: uploadFile :: error :: ", error);
+            return false;
+        }
+    }
+
+
+    async deleteFile(fileId){
+        try {
+            await this.bucket.deleteFile(
+                config.appwriteBucketId,
+                fileId
+            )
+            return true;
+        } catch (error) {
+            console.log("Appwrite service :: deleteFile :: error :: ", error);
+            return false;
+        }
+    }
+
+    getFilePreview(fileId){
+        return this.bucket.getFilePreview(
+            config.appwriteBucketId,
+            fileId
+        )
     }
 }
